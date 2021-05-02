ba4c24e854406b78b5cb45de87c7d966471a9372
Hidel <hidelberto.cayo57@gmail.com>
2021-05-01 20:31:43 -0400

    second commit

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 function showNavbar (hamburguerId, navId) {
   const hamburguer = document.getElementById(hamburguerId)
   const nav = document.getElementById(navId)
+
   if (hamburguer && nav) {
     hamburguer.addEventListener('click', () => {
       nav.classList.toggle('navshow')
@@ -8,4 +9,56 @@ function showNavbar (hamburguerId, navId) {
   }
 }
 
+function closedNavbar (menuListClass, navId) {
+  const menuLink0 = document.getElementsByClassName(menuListClass)[0]
+  const menuLink1 = document.getElementsByClassName(menuListClass)[1]
+  const menuLink2 = document.getElementsByClassName(menuListClass)[2]
+  const menuLink3 = document.getElementsByClassName(menuListClass)[3]
+  const menuLink4 = document.getElementsByClassName(menuListClass)[4]
+  const nav = document.getElementById(navId)
+  if (menuLink0 && nav) {
+    menuLink0.addEventListener('click', () => {
+      nav.classList.remove('navshow')
+      menuLink0.children[0].classList.toggle('link--actived')
+      menuLink1.children[0].classList.remove('link--actived')
+      menuLink2.children[0].classList.remove('link--actived')
+      menuLink3.children[0].classList.remove('link--actived')
+      menuLink4.children[0].classList.remove('link--actived')
+    })
+    menuLink1.addEventListener('click', () => {
+      nav.classList.remove('navshow')
+      menuLink0.children[0].classList.remove('link--actived')
+      menuLink1.children[0].classList.toggle('link--actived')
+      menuLink2.children[0].classList.remove('link--actived')
+      menuLink3.children[0].classList.remove('link--actived')
+      menuLink4.children[0].classList.remove('link--actived')
+    })
+    menuLink2.addEventListener('click', () => {
+      nav.classList.remove('navshow')
+      menuLink0.children[0].classList.remove('link--actived')
+      menuLink1.children[0].classList.remove('link--actived')
+      menuLink2.children[0].classList.toggle('link--actived')
+      menuLink3.children[0].classList.remove('link--actived')
+      menuLink4.children[0].classList.remove('link--actived')
+    })
+    menuLink3.addEventListener('click', () => {
+      nav.classList.remove('navshow')
+      menuLink0.children[0].classList.remove('link--actived')
+      menuLink1.children[0].classList.remove('link--actived')
+      menuLink2.children[0].classList.remove('link--actived')
+      menuLink3.children[0].classList.toggle('link--actived')
+      menuLink4.children[0].classList.remove('link--actived')
+    })
+    menuLink4.addEventListener('click', () => {
+      nav.classList.remove('navshow')
+      menuLink0.children[0].classList.remove('link--actived')
+      menuLink1.children[0].classList.remove('link--actived')
+      menuLink2.children[0].classList.remove('link--actived')
+      menuLink3.children[0].classList.remove('link--actived')
+      menuLink4.children[0].classList.toggle('link--actived')
+    })
+  }
+}
+
 showNavbar('hamburger', 'nav')
+closedNavbar('menu--list', 'nav')
